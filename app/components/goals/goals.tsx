ca4f932c1eb3f44bd819969c180d73ea7930842a
KaiSrdoc <kai@srdoc.si>
2025-05-25 18:17:58 +0200

    chore: move goals title and button to component

--- app/components/goals/goals.tsx
+++ app/components/goals/goals.tsx
@@ -1,17 +1,31 @@
-import { Stack } from "@mantine/core";
+import { Button, Group, Stack, Title } from "@mantine/core";
 import { useWorkStore } from "../../work.store";
 import { GoalProgress } from "./goal-progress";
 import { GoalForm } from "./goal-form";
+import { IconPlus } from "@tabler/icons-react";
 
 export function Goals() {
-  const { goals } = useWorkStore();
+  const { goals, openGoalForm } = useWorkStore();
 
   return (
-    <Stack>
-      {goals.map((goal) => (
-        <GoalProgress key={goal.id} goal={goal} />
-      ))}
-      <GoalForm />
+    <Stack gap="0">
+      <Group justify="space-between" align="center" mb="xl">
+        <Title order={1} ta="left">
+          Kai&apos;s Goals
+        </Title>
+        <Button
+          leftSection={<IconPlus size={16} />}
+          onClick={() => openGoalForm()}
+        >
+          Add Goal
+        </Button>
+      </Group>
+      <Stack>
+        {goals.map((goal) => (
+          <GoalProgress key={goal.id} goal={goal} />
+        ))}
+        <GoalForm />
+      </Stack>
     </Stack>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,12 @@
 "use client";
 
-import { Container, Title, Group, Button } from "@mantine/core";
-import { IconPlus } from "@tabler/icons-react";
+import { Container } from "@mantine/core";
 import { Goals } from "./components/goals/goals";
 import Work from "./components/work/work";
-import { useWorkStore } from "./work.store";
 
 export default function Home() {
-  const { openGoalForm } = useWorkStore();
-
   return (
     <Container size="lg" py="xl">
-      <Group justify="space-between" align="center" mb="xl">
-        <Title order={1} ta="left">
-          Kai&apos;s Work
-        </Title>
-        <Button
-          leftSection={<IconPlus size={16} />}
-          onClick={() => openGoalForm()}
-        >
-          Add Goal
-        </Button>
-      </Group>
-
       <Goals />
       <Work />
     </Container>
